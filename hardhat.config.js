b44257e93b3d321fa58f5902682aff803fdca6b4
Zubin Pratap <zubin.pratap@gmail.com>
2022-05-17 19:09:01 +1000

    update hardhat config compilers

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.6.4",
+      },
+    ],
+  },
 };
