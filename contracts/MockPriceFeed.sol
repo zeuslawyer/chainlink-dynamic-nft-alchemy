7c02618dee5987cb47228cd553f525d3acc98eac
Zubin Pratap <zubin.pratap@gmail.com>
2022-05-17 18:50:54 +1000

    update hardhat config for multiple compilers. Shorten MockPriceFeed to import only

---
+++ contracts/MockPriceFeed.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
+
+// This file create a MockAggregator (price feed smart contract) for testing.
